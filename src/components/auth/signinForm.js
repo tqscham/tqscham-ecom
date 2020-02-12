c1b8275617d8b094f10a6275c1d4df021325efc0
tqscham <tqscham@outlook.com>
2020-02-11 21:56:27 -0600

    built the details component for quicklinks

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 import { FormInput, FormButton } from '../formFields';
+import Details from '../details';
+import history from '../../history';
 
 
 class SignInForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
+        const links =[
+            {
+                _id: 0,
+                title: 'Not Registered? Create account here.',
+                onClick: () => history.push('/signup')
+            },
+            {
+                _id: 1,
+                title: 'Forgot accoutn email?',
+                onClick: () => console.log('forgot email')
+            },
+            {
+                _id: 2,
+                title: 'Forgot password?',
+                onClick: () => console.log('forgot password')
+            }
+        ]
 
         return (
             <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
@@ -32,6 +51,7 @@ class SignInForm extends Component {
                 type='submit' 
                 title='Login'
                 component={FormButton}/>
+                <Details className='sing-in-form__details' title='QuickLinks' links={links} />
             </form>
         );
     }
