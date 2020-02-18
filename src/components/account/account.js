fd1c20d1d3371626b2b854758b5c3d2c2ca7e26b
tqscham <tqscham@outlook.com>
2020-02-18 17:41:47 -0600

    setup purchase history component

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -26,13 +26,13 @@ class Account extends Component {
             {
                 _id: 0,
                 title: 'Purchase History',
-                active: false,
+                active: true,
                 component: <PurchaseHistory />
             },
             {
                 _id: 1,
                 title: 'Account Information',
-                active: true,
+                active: false,
                 component: <AccountInformation />
             }
         ]
@@ -55,7 +55,7 @@ class Account extends Component {
 
     render() {
         return (
-            <div>
+            <div className='account'>
                 { this.renderContent() }
             </div>
         );
