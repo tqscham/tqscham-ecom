4ff6c33e749f9654ea3f37de5c5e5a898e6b8f92
tqscham <tqscham@outlook.com>
2020-02-18 19:02:24 -0600

    styled purchase history components

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,13 +8,99 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: 'purchase 1',
-                amount: 8.00
+                total: 8.00,
+                orderNumber: 'A00123',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Taylor Schamberger',
+                    shippingAddress: '1234 West State Street',
+                }
+
             },
             {
                 _id: 1,
-                title: 'purchase 2',
-                amount: 16.35
+                total: 19.35,
+                orderNumber: 'A00134',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Lyra Schamberger',
+                    shippingAddress: '1234 West State Street',
+                }
+
+            },
+            {
+                _id: 2,
+                total: 81.00,
+                orderNumber: 'A00145',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Toby Schamberger',
+                    shippingAddress: '1234 West State Street',
+                }
+
+            },
+            {
+                _id: 3,
+                total: 4.33,
+                orderNumber: 'A00156',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Zach Northy',
+                    shippingAddress: '1234 West State Street',
+                }
+
+            },
+            {
+                _id: 4,
+                total: 17.48,
+                orderNumber: 'A00167',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Q-Ball',
+                    shippingAddress: '1234 West State Street',
+                }
+
+            },
+            {
+                _id: 5,
+                total: 32.00,
+                orderNumber: 'A00178',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Dan Manson',
+                    shippingAddress: '1234 West State Street',
+                }
+
+            },
+            {
+                _id: 6,
+                total: 12.35,
+                orderNumber: 'A00189',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Timmothy',
+                    shippingAddress: '1234 West State Street',
+                }
+
+            },
+            {
+                _id: 7,
+                total: 8.00,
+                orderNumber: 'A00190',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jeff Jeffersonson',
+                    shippingAddress: '1234 West State Street',
+                }
+
             }
         ]
     })
