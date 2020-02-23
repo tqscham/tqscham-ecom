3446ccf36c6ad7ff038522e5925bdec967a29f44
tqscham <tqscham@outlook.com>
2020-02-23 13:43:44 -0600

    formatted input layout for accountInformation

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -12,20 +12,55 @@ class AccountInformationForm extends Component {
 
         return (
             <form onSubmit={handleSubmit} className={`${className} account-information-form`}>
+                <Field className='account-information-form__name' 
+                name='name' 
+                type='name' 
+                title='Name' 
+                placeholder='Name' 
+                component={FormInput}/>
+                
                 <Field className='account-information-form__email' 
                 name='email' 
                 type='email' 
                 title='Email' 
                 placeholder='Email' 
                 component={FormInput}/>
+                
+                <Field className='account-information-form__street-address' 
+                name='address' 
+                type='address' 
+                title='Street Address' 
+                placeholder='Street Address' 
+                component={FormInput}/>
+                
+                <Field className='account-information-form__city' 
+                name='city' 
+                type='city' 
+                title='City' 
+                placeholder='City' 
+                component={FormInput}/>
+                
+                <Field className='account-information-form__state' 
+                name='state' 
+                type='state' 
+                title='State' 
+                placeholder='State' 
+                component={FormInput}/>
+                
+                <Field className='account-information-form__zipcode' 
+                name='zipcode' 
+                type='zipcode' 
+                title='Zipcode' 
+                placeholder='Zipcode' 
+                component={FormInput}/>
 
-                <Field className='account-information-form__password' 
+                {/* <Field className='account-information-form__password' 
                 name='password' 
                 type='password' 
                 title='Password' 
                 placeholder='Password' 
                 component={FormInput}/>
-
+                
                 <div className='account-information-form__line'></div>
                 
                 <Field className='account-information-form__login' 
@@ -33,7 +68,7 @@ class AccountInformationForm extends Component {
                 name='login' 
                 type='submit' 
                 title='Login'
-                component={FormButton}/>
+                component={FormButton}/> */}
             </form>
         );
     }
