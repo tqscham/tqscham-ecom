5d1aef916f94fc33c0637ec9bf0dd2bb2ed372c2
tqscham <tqscham@outlook.com>
2020-03-01 15:32:50 -0600

    started building review form

---
+++ src/components/order/reviewForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+import { FormButton } from '../formFields';
+import history from '../../history';
+
+
+class ReviewForm extends Component {
+    render() {
+        const { className, handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit} className={`${className} review-form`}>
+                
+
+                <div className='review-form__line'></div>
+                
+                <Field className='review-form__proceed' 
+                onClick={() => history.push('/account')}
+                name='proceed' 
+                type='submit' 
+                title='Proceed to Checkout'
+                component={FormButton}/>
+                
+                <Field className='review-form__back' 
+                onClick={() => history.push('/shop')}
+                name='back' 
+                type='button' 
+                title='Back'
+                short={true}
+                component={FormButton}/>
+            </form>
+        );
+    }
+}
+
+ReviewForm = reduxForm({
+    form: 'ReviewForm'
+})(ReviewForm);
+
+export default ReviewForm;
