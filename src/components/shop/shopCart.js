ce41a334620f5d946f25f0d2355a78df8969cb46
tqscham <tqscham@outlook.com>
2020-02-29 19:51:50 -0600

    built shop-cart-content-footer component and styled the checkout button

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -10,19 +10,35 @@ function CartButton ({ className, symbol }) {
     )
 }
 
+function CartFooter ({className, products}) {
+    const price = 420.69;
+    return (
+        <div className={`${className} cart-footer`}>
+            <a className='cart-footer__checkout'>
+                Checkout
+            </a>
+            <div className='cart-footer__subtotal'>
+                Subtotal
+            </div>
+            <div className='cart-footer__price'>
+                ${price}
+            </div>
+        </div>
+    )
+}
+
 function CartContent({className, products}){
     let count = products.length;
+    let productsJSX = products.map(product => <h1 key={product} >{product}</h1>)
     return (
         <div className={`${className} cart-content`}>
             <div className='cart-content__title'>
                 Cart ({count})
             </div>
             <div className='cart-content__products'>
-
+                {productsJSX}
             </div>
-            <div className='cart-content__footer'>
-
-            </div>
+            <CartFooter className='cart-content__footer' products={products} />
         </div>
     )
 }
